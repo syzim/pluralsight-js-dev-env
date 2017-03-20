212873d2c5ec4b33c24388bec24adbabdd3397f4
Kieran Sisam <Kieran Sisam>
2017-03-20 15:56:39 +1300

    mock api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './index.css';
 import numeral from 'numeral';
-import {getUsers} from './api/userApi';
+import {getUsers, deleteUser} from './api/userApi';
 /*eslint-disable no-console*/
 
 const courseValue = numeral(1000).format('$0,0.00');
@@ -19,5 +19,24 @@ getUsers().then(result => {
     <td>${user.email}</td>
     </tr>`
   });
-  global.document.getElementById('users').innerHTML = usersBody;
+
+
+
+
+
+    global.document.getElementById('users').innerHTML = usersBody;
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+
+  Array.from(deleteLinks,link => {
+    link.onclick = function(event){
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    };
 });
+
+  });
